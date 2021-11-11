398fc614d6e4e7221435bc506487edf55c0b9efc
delfino <dechicchisd@gmail.com>
2021-11-11 19:47:08 +0100

    aggiunto docker

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
-console.log(Koa);;
+const app = new Koa();
+const router = new Router();
+
+
+router.get('/', ctx =>{ ctx.body = "Hello World"; })
+
+router.get('/test', ctx => { ctx.body = 'Hello Test '});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+
+app.listen(3000, () => console.log('Server Started...'));

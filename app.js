2983618876d7328405256e98f24d5a78c8a10a5d
delfino <dechicchisd@gmail.com>
2021-11-12 15:31:54 +0100

    aggiunto gitignore

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = new Koa();
 const router = new Router();
 
 
-router.get('/', ctx =>{ ctx.body = "Hello World"; })
+router.get('/', ctx => { ctx.body = "Hello World" });
 
-router.get('/test', ctx => { ctx.body = 'Hello Test '});
+router.get('/test', ctx => { ctx.body = 'Hello Test' });
 
 app.use(router.routes()).use(router.allowedMethods());
 

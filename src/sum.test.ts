9cbcc10e1b275e6ae7adcc6f318667db4b52c99c
dechicchisd <72296964+dechicchisd@users.noreply.github.com>
2021-11-22 19:31:17 +0100

    change all the require statement into import (#2)
    * changed all the require statement into import
    
    * delete comments and fix import

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,11 +1,13 @@
-const testSum = require('./sum');
+import { sum } from './sum'
 
 describe('SOMMA', () => {
     test('somma di due interi positivi', () => {
-        expect(testSum(3, 5)).toBe(8);
+        expect(sum(3, 5)).toBe(8);
     })
 
     test('somma di due numeri qualsiasi', () => {
-        expect(testSum(3, -5)).toBe(-2);
+        expect(sum(3, -5)).toBe(-2);
     })
 });
+
+

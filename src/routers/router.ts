ed6146ff938c11f307c7b42e4c454d75253d1db1
dechicchisd <dechicchisd@gmail.com>
2021-11-29 00:32:09 +0100

    test http request

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,14 +1,19 @@
+import Koa from "koa"
 import Router from "koa-router"
 import { sum } from "../services/sum"
 
 export const getRouter = () => {
     const router = new Router();
 
-    router.get('/', ctx => {
+    router.get('/', (ctx: Koa.Context) => {
         ctx.body = 'Hello World';
     });
 
-    router.get('/test/:num1/:num2', ctx => {
+    router.post('/', (ctx: Koa.Context) => {
+        ctx.body = ctx.request.body;
+    });
+
+    router.get('/test/:num1/:num2', (ctx: Koa.Context) => {
         ctx.body = 'Hello test ' + sum(Number(ctx.params.num1), Number(ctx.params.num2));
     });
 

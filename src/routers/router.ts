1a94f60e402568cc0f4942a1d8c1cdf080b9cdc2
delfino <dechicchisd@gmail.com>
2021-11-29 14:28:49 +0100

    test put and delete

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -2,15 +2,32 @@ import Koa from "koa"
 import Router from "koa-router"
 import { sum } from "../services/sum"
 
+let users = [{
+    name: 'Delfino',
+    surname: 'De Chicchis'
+},
+
+{
+    name: 'Name1',
+    surname: 'Surname1'
+},
+
+{
+    name: 'Name2',
+    surname: 'Surname2'
+}
+]
+
 export const getRouter = () => {
     const router = new Router();
 
-    router.get('/', (ctx: Koa.Context) => {
+    router.all('/', (ctx: Koa.Context) => {
         ctx.body = 'Hello World';
     });
 
-    router.post('/', (ctx: Koa.Context) => {
-        ctx.body = ctx.request.body;
+    router.delete('/:id', (ctx: Koa.Context) => {
+        users.splice(ctx.params.id, 1);
+        ctx.body = users;
     });
 
     router.get('/test/:num1/:num2', (ctx: Koa.Context) => {

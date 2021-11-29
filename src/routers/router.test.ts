a79e1075f97c1279d4e54927a5e7f92d0d4bb4a6
delfino <dechicchisd@gmail.com>
2021-11-29 14:28:49 +0100

    test put and delete

--- src/routers/router.test.ts
+++ src/routers/router.test.ts
@@ -6,12 +6,12 @@ const app = getApp();
 describe('POST', () => {
     it('POST success', async () => {
         const response = await request(app.callback()).post('/').send({
-            username: 'username',
-            password: 'password'
+            data: 'something',
         })
         expect(response.status).toEqual(200)
-    })
+        expect(response.text).toEqual('Hello World')
 
+    })
 })
 
 describe('GET', () => {
@@ -20,5 +20,44 @@ describe('GET', () => {
         expect(response.status).toEqual(200)
         expect(response.text).toEqual('Hello World')
     })
+})
 
+describe('PUT', () => {
+    it('PUT success', async () => {
+        const response = await request(app.callback()).put('/').send({
+            data: 'somthing',
+        })
+        expect(response.status).toEqual(200)
+        expect(response.text).toEqual('Hello World')
+
+    })
+})
+
+describe('DELETE', () => {
+    let users;
+
+    beforeAll(() => {
+        users = [{
+            name: 'Delfino',
+            surname: 'De Chicchis'
+        },
+
+        {
+            name: 'Name1',
+            surname: 'Surname1'
+        },
+
+        {
+            name: 'Name2',
+            surname: 'Surname2'
+        }
+        ]
+    })
+    it('DELETE success by deleting first user from users', async () => {
+        let userTest = users;
+        const response = await request(app.callback()).delete('/0');
+        expect(response.status).toEqual(200);
+        userTest.shift();
+        expect(users).toBe(userTest);
+    })
 })

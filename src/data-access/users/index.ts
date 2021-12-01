8c9c8c90165ee3d1ac8a564c5545dd5a5b0e1a62
dechicchisd <72296964+dechicchisd@users.noreply.github.com>
2021-12-01 18:03:17 +0100

    refactoring crud operations (#6)
    * refactoring crud operations
    
    * deleteUser returns both body and status

---
+++ src/data-access/users/index.ts
@@ -0,0 +1,18 @@
+export let users = [{
+    id: 1,
+    name: 'a',
+    surname: 'b'
+},
+
+{
+    id: 2,
+    name: 'c',
+    surname: 'd'
+},
+
+{
+    id: 3,
+    name: 'e',
+    surname: 'f'
+}
+]
---
+++ src/routers/usersRouter.ts
@@ -0,0 +1,24 @@
+import Koa from "koa"
+import Router from "koa-router"
+import { users } from "../data-access/users/index"
+import { deleteUser } from "../services/users"
+
+export let getUsersRouter = () => {
+    const router: Router = new Router();
+
+    router.get('/getAllUsers');
+
+    router.post('/createUser');
+
+    router.put('/putUser');
+
+    router.get('/user/:id');
+
+    router.delete('/user/:id', (ctx: Koa.Context) => {
+        const result = deleteUser(users, ctx.params.id);
+        ctx.body = result.deleteBody;
+        ctx.status = result.deleteStatus;
+    });
+
+    return router;
+}

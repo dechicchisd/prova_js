b7812cb6d23af9b47f37d242b15656114a9879d9
Giorgio27 <giopag27@gmail.com>
2021-12-01 08:32:14 +0100

    add get users endpoint

--- src/data-access/users/index.ts
+++ src/data-access/users/index.ts
@@ -1,19 +1,17 @@
-export let users = [
-  {
-    id: 1,
-    name: 'a',
-    surname: 'b',
-  },
+import {promises as fs} from 'fs'
+import path from 'path'
+import {User} from '../../model/users/schema'
 
-  {
-    id: 2,
-    name: 'c',
-    surname: 'd',
-  },
-
-  {
-    id: 3,
-    name: 'e',
-    surname: 'f',
-  },
-]
+export const getUsersFromFile = async (): Promise<User[]> => {
+  const filePath = path.resolve(__dirname, `./data.txt`)
+  const fileData = await fs.readFile(filePath, 'utf-8')
+  const fileLines = fileData.split('\n')
+  return fileLines.map((line: string) => {
+    const splittedLine = line.split('|')
+    return {
+      id: +splittedLine[0],
+      name: splittedLine[1],
+      surname: splittedLine[2],
+    }
+  })
+}
---
+++ src/routers/usersRouter.test.ts
@@ -0,0 +1,51 @@
+import {getApp} from '../servers/server'
+import request from 'supertest'
+
+const app = getApp()
+
+describe('userRouter', () => {
+  describe('getAllUsers', () => {
+    it('should return all the users', async () => {
+      const response = await request(app.callback()).get('/users')
+      expect(response.status).toEqual(200)
+      expect(response.body).toEqual({
+        users: [
+          {id: 1, name: 'name1', surname: 'surname1'},
+          {id: 2, name: 'name2', surname: 'surname2'},
+          {id: 3, name: 'name3', surname: 'surname3'},
+        ],
+      })
+    })
+  })
+
+  describe('deleteUser', () => {
+    it('DELETE user not found', async () => {
+      const response = await request(app.callback()).delete('/users/0')
+      expect(response.status).toEqual(404)
+    })
+
+    it('DELETE success by deleting first user from users', async () => {
+      const response = await request(app.callback()).delete('/users/1')
+      expect(response.status).toEqual(200)
+      expect(response.body).toEqual([
+        {
+          id: 1,
+          name: 'name1',
+          surname: 'surname1',
+        },
+      ])
+    })
+
+    it('DELETE the same user two times', async () => {
+      let response = await request(app.callback()).delete('/users/2')
+      expect(response.status).toEqual(200)
+      expect(response.body).toEqual([
+        {
+          id: 2,
+          name: 'name2',
+          surname: 'surname2',
+        },
+      ])
+    })
+  })
+})

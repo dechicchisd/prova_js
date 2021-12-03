154223d8091c2205b9246fd130a8462c8c21ea1e
dechicchisd <dechicchisd@gmail.com>
2021-12-03 14:29:15 +0100

    delete utils directory

--- src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export let splitName = (urlName: string) => {
-    let nameAndSurname = urlName.split('+')
-
-    return {
-        name: nameAndSurname[0].replace('-', ' '),
-        surname: nameAndSurname[1].replace('-', ' ')
-    }
-}
